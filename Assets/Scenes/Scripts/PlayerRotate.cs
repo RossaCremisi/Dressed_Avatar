e6ad3583b73d478e89b75fe2ee2786780a83a5b8
Francesco-Platania <61884192+RossaCremisi@users.noreply.github.com>
2022-01-19 13:32:17 +0100

    PochoAndRotatepg
    Now the two features work on the build

--- Assets/Scenes/Scripts/PlayerRotate.cs
+++ Assets/Scenes/Scripts/PlayerRotate.cs
@@ -5,31 +5,21 @@ using UnityEngine;
 public class PlayerRotate : MonoBehaviour
 {
     private Touch touch;
-
-    private Vector2 touchPosition;
-
-    private Quaternion rotationY;
-
-    private float rotateSpeed = 0.2f;
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
+    public Quaternion rotationX;
+    public float rotateSpeedModifier = 0.1f;
+    public float rot;
     // Update is called once per frame
     void Update()
     {
-        if(Input.touchCount > 0)
+
+        if (Input.touchCount > 0)
         {
             touch = Input.GetTouch(0);
-
             if (touch.phase == TouchPhase.Moved)
             {
 
-                rotationY = Quaternion.Euler(0f,- touchPosition.x * rotateSpeed, 0f);
-                transform.rotation = rotationY* transform.rotation;
-                Debug.Log("Entrato");
+                rotationX = Quaternion.Euler(0f, -touch.deltaPosition.x * rotateSpeedModifier, 0);
+                transform.rotation = rotationX * transform.rotation;
             }
         }
     }
